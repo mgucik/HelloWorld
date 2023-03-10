39a0422e87c53c014e60ea1901c0b9952cdcedad
Mariusz Gucik <Mariusz Gucik@LAPTOP-3ICJFC9S>
2023-03-10 18:29:54 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imie = "Mariusz";
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Nazywam się Mariusz Gucik");
+        }
+    }
+}
+ 
+
